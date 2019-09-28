f3033acf0d1b3093f99ee20a46c9eafbc21d346e
LuisAGC <luisantoniogarciacastaneda@gmail.com>
2019-09-27 23:33:18 -0500

    Initial commit, have working driver

---
+++ max7219.c
@@ -0,0 +1,101 @@
+#include <max7219.h>
+
+void max7219_SetAmountOfDisplays(unsigned int8 amount){
+   max7219_numberOfDisplays = amount;
+}
+
+void max7219_TurnOnAllDisplays(){
+   unsigned int8 counter = 0;
+   output_low(LOAD_PIN);
+   for(counter = 0; counter < max7219_numberOfDisplays; counter++){
+      spi_write(MAX7219_SHUTDOWN_REG);
+      spi_write(MAX7219_NOSHUTDOWN);
+   }
+   output_high(LOAD_PIN);
+   output_low(LOAD_PIN);
+}
+
+void max7219_TestAllDisplays(int1 test){
+   unsigned int8 counter = 0;
+   output_low(LOAD_PIN);
+   for(counter = 0; counter < max7219_numberOfDisplays; counter++){
+      spi_write(MAX7219_TESTDISPLAY_REG);
+      spi_write(test);
+   }
+   output_high(LOAD_PIN);
+   output_low(LOAD_PIN);
+}
+
+int1 max7219_SetDigitsDecode(unsigned int8 displayNumber, unsigned int8 digitSetup){
+   if(displayNumber >= max7219_numberOfDisplays) return FALSE;
+   unsigned int8 counter = 0;
+   output_low(LOAD_PIN);
+   for(counter = 0; counter < max7219_numberOfDisplays; counter++){
+      if(counter == displayNumber){
+         spi_write(MAX7219_DECODEMODE_REG);
+         spi_write(digitSetup);
+      }
+      else{
+         spi_write(MAX7219_NOOP);
+         spi_write(MAX7219_NOOP);
+      }
+   }
+   output_high(LOAD_PIN);
+   output_low(LOAD_PIN);
+   return TRUE;
+}
+
+int1 max7219_SetDigitValue(unsigned int8 displayNumber, unsigned int8 digitRegister, unsigned int8 digitValue){
+   if(displayNumber >= max7219_numberOfDisplays) return FALSE;
+   unsigned int8 counter = 0;
+   output_low(LOAD_PIN);
+   for(counter = 0; counter < max7219_numberOfDisplays; counter++){
+      if(counter == displayNumber){
+         spi_write(digitRegister);
+         spi_write(digitValue);
+      }
+      else{
+         spi_write(MAX7219_NOOP);
+         spi_write(MAX7219_NOOP);
+      }
+   }
+   output_high(LOAD_PIN);
+   output_low(LOAD_PIN);
+   return TRUE;
+}
+
+void max7219_ClearAllDisplays(){
+   unsigned int8 counter = 0;
+   unsigned int8 digit = 0;
+   output_low(LOAD_PIN);
+   for(digit = MAX7219_D0_REG; digit <= MAX7219_D7_REG; digit++){
+      for(counter = 0; counter < max7219_numberOfDisplays; counter++){
+         spi_write(digit);
+         spi_write(MAX7219_NOOP);
+      }
+      output_high(LOAD_PIN);
+      output_low(LOAD_PIN);
+   }
+}
+
+void max7219_ScanAllDisplays(){
+   unsigned int8 counter = 0;
+   output_low(LOAD_PIN);
+   for(counter = 0; counter < max7219_numberOfDisplays; counter++){
+      spi_write(MAX7219_SCANLIMIT_REG);
+      spi_write(MAX7219_SCAN_ALL);
+   }
+   output_high(LOAD_PIN);
+   output_low(LOAD_PIN);
+}
+
+void max7219_SetAllDisplaysIntensity(unsigned int8 intensity){
+   unsigned int8 counter = 0;
+   output_low(LOAD_PIN);
+   for(counter = 0; counter < max7219_numberOfDisplays; counter++){
+      spi_write(MAX7219_INTENSITY_REG);
+      spi_write(intensity);
+   }
+   output_high(LOAD_PIN);
+   output_low(LOAD_PIN);
+}
---
+++ max7219.h
@@ -0,0 +1,108 @@
+
+/***************************************
+The following section is to define the
+MAX7219 register addresses
+*****************************************/
+#define MAX7219_NOOP                0x00
+#define MAX7219_D0_REG              0x01
+#define MAX7219_D1_REG              0x02
+#define MAX7219_D2_REG              0x03
+#define MAX7219_D3_REG              0x04
+#define MAX7219_D4_REG              0x05
+#define MAX7219_D5_REG              0x06
+#define MAX7219_D6_REG              0x07
+#define MAX7219_D7_REG              0x08
+#define MAX7219_DECODEMODE_REG      0x09
+#define MAX7219_INTENSITY_REG       0x0A
+#define MAX7219_SCANLIMIT_REG       0x0B
+#define MAX7219_SHUTDOWN_REG        0x0C
+#define MAX7219_TESTDISPLAY_REG     0x0F
+
+/****************************************
+The following section is to define the
+MAX7219 shutdown register operations
+available
+*****************************************/
+#define MAX7219_SHUTDOWN            0x00
+#define MAX7219_NOSHUTDOWN          0x01
+
+/****************************************
+The following section is to define the
+MAX7219 decode values for each
+individual digits. Decoding is BCD format
+*****************************************/
+#define MAX7219_D0_DECODE           0x01
+#define MAX7219_D1_DECODE           0x02
+#define MAX7219_D2_DECODE           0x04
+#define MAX7219_D3_DECODE           0x08
+#define MAX7219_D4_DECODE           0x10
+#define MAX7219_D5_DECODE           0x20
+#define MAX7219_D6_DECODE           0x40
+#define MAX7219_D7_DECODE           0x80
+#define MAX7219_ALL_DECODE          0xFF
+
+/****************************************
+The following section is to define the
+MAX7219 decode character values. 
+Decoding is BCD format
+*****************************************/
+#define MAX7219_BCD_0               0x00
+#define MAX7219_BCD_1               0x01
+#define MAX7219_BCD_2               0x02
+#define MAX7219_BCD_3               0x03
+#define MAX7219_BCD_4               0x04
+#define MAX7219_BCD_5               0x05
+#define MAX7219_BCD_6               0x06
+#define MAX7219_BCD_7               0x07
+#define MAX7219_BCD_8               0x08
+#define MAX7219_BCD_9               0x09
+#define MAX7219_BCD_MINUS           0x0A
+#define MAX7219_BCD_E               0x0B
+#define MAX7219_BCD_H               0x0C
+#define MAX7219_BCD_L               0x0D
+#define MAX7219_BCD_P               0x0E
+#define MAX7219_BCD_BLANK           0x0F
+#define MAX7219_BCD_DOT             0x80
+
+/****************************************
+The following section is to define the
+MAX7219 shutdown register operations
+available
+*****************************************/
+#define MAX7219_NOTEST              0x00
+#define MAX7219_TEST                0x01
+
+/****************************************
+The following section is to define the
+MAX7219 scan digit register operations
+available
+*****************************************/
+#define MAX7219_SCAN_0              0x00
+#define MAX7219_SCAN_0_1            0x01
+#define MAX7219_SCAN_0_1_2          0x02
+#define MAX7219_SCAN_0_1_2_3        0x03
+#define MAX7219_SCAN_0_1_2_3_4      0x04
+#define MAX7219_SCAN_0_1_2_3_4_5    0x05
+#define MAX7219_SCAN_0_1_2_3_4_5_6  0x06
+#define MAX7219_SCAN_ALL            0x07
+
+
+#ifndef LOAD_PIN
+   #define LOAD_PIN PIN_C0
+#endif
+
+//Variables needed to work
+unsigned int8 max7219_numberOfDisplays = 0;
+
+void max7219_SetAmountOfDisplays(unsigned int8);
+void max7219_TurnOnAllDisplays(void);
+int1 max7219_TurnOnDisplay(unsigned int8);
+void max7219_TestAllDisplays(int1);
+int1 max7219_TestDisplay(unsigned int8, int1);
+int1 max7219_SetDigitsDecode(unsigned int8, unsigned int8);
+int1 max7219_SetDigitValue(unsigned int8, unsigned int8, unsigned int8);
+void max7219_ClearAllDisplays(void);
+int1 max7219_ClearDisplay(unsigned int8);
+void max7219_ScanAllDisplays(void);
+int1 max7219_ScanDisplayDigits(unsigned int8, unsigned int8);
+void max7219_SetAllDisplaysIntensity(unsigned int8);
